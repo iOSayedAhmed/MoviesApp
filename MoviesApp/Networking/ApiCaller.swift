2b2be21470a7cbac72397948711c71c5bbffb22f
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2023-03-05 22:33:20 +0200

    Create ApiCaller.swift

---
+++ MoviesApp/Networking/ApiCaller.swift
@@ -0,0 +1,18 @@
+//
+//  ApiCaller.swift
+//  MoviesApp
+//
+//  Created by iOSayed on 05/03/2023.
+//
+
+import Foundation
+
+enum NetworkError:Error {
+    case urlError
+    case parsingError
+}
+
+
+    
+    
+
