403e0cb870ae188707970bac6ea334c4afec56ec
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2023-03-05 22:33:28 +0200

    Create MoviesTrendingModel.swift

---
+++ MoviesApp/Models/MoviesTrendingModel.swift
@@ -0,0 +1,68 @@
+//
+//  MoviesTrendingModel.swift
+//  MoviesApp
+//
+//  Created by iOSayed on 05/03/2023.
+//
+
+import Foundation
+
+// MARK: - MoviesTrendingModel
+struct MoviesTrendingModel: Codable {
+    let page: Int
+    let results: [Result]
+    let totalPages, totalResults: Int
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let adult: Bool
+    let backdropPath: String
+    let id: Int
+    let name: String?
+    let originalLanguage: String
+    let originalName: String?
+    let overview, posterPath: String
+    let mediaType: MediaType
+    let genreIDS: [Int]
+    let popularity: Double
+    let firstAirDate: String?
+    let voteAverage: Double
+    let voteCount: Int
+    let originCountry: [String]?
+    let title, originalTitle, releaseDate: String?
+    let video: Bool?
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case id, name
+        case originalLanguage = "original_language"
+        case originalName = "original_name"
+        case overview
+        case posterPath = "poster_path"
+        case mediaType = "media_type"
+        case genreIDS = "genre_ids"
+        case popularity
+        case firstAirDate = "first_air_date"
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+        case originCountry = "origin_country"
+        case title
+        case originalTitle = "original_title"
+        case releaseDate = "release_date"
+        case video
+    }
+}
+
+enum MediaType: String, Codable {
+    case movie = "movie"
+    case tv = "tv"
+}
+
